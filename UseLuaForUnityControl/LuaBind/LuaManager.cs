f97721314ec0dd15c067f07f9e237414740d92a1
yamashita8655 <wisdomword-justice@hotmail.co.jp>
2015-02-23 17:14:10 +0900

    成果物コミット。単体では動かないが、こういう事をしたよというメモ。 今後実装する時のメモになる。

---
+++ UseLuaForUnityControl/LuaBind/LuaManager.cs
@@ -0,0 +1,321 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using LuaDLLTest;
+
+public class LuaManager : SingletonMonoBehaviour<LuaManager>{
+
+	public class FunctionData
+	{
+		public int returnValueNum;	// 戻り値の数
+		public string functionName;	// 呼び出したい関数名
+		public ArrayList argList;	// 引数リスト。これがあれば、引数の数も取得可能
+	}
+
+	public class TableData
+	{
+		public ArrayList tableDataList;// 作成するテーブルに使うデータ。文字列か数値になるはず
+		public TableData()
+		{
+			tableDataList = new ArrayList ();
+		}
+	}
+
+	// Luaが解釈できる関数ポインタの型
+	public delegate int DelegateLuaBindFunction(IntPtr luaState);
+
+	// Luaの読み込み状態のマップ
+	Dictionary<string, IntPtr> mLuaStateMap = null;
+	
+	// LuaにUnityの関数を登録した時、実体を覚えておかないとガベコレで破棄されて
+	// Lua側から呼び出そうとした時に無効になるので、登録した関数の実体を覚えておくリスト
+	Dictionary<string, DelegateLuaBindFunction> mDelegateAddressMap = null;
+
+	
+	void Awake () {
+		base.Awake ();
+		DontDestroyOnLoad (this);
+	}
+	
+	public void Init()
+	{
+		if (mLuaStateMap == null) 
+		{
+			mLuaStateMap = new Dictionary<string, IntPtr> ();
+		}
+
+		if (mDelegateAddressMap == null) 
+		{
+			mDelegateAddressMap = new Dictionary<string, DelegateLuaBindFunction> ();
+		}
+	}
+
+	// LuaStateを使えるようにする初期化みたいな物
+	public IntPtr GetLuaState(string filename)
+	{
+		// すでに登録されている物か、確認する
+		if(mLuaStateMap.ContainsKey(filename) == true)
+		{
+			return mLuaStateMap[filename];
+		}
+
+		IntPtr luastate = NativeMethods.luaL_newstate();
+		NativeMethods.luaL_openlibs(luastate);
+
+		mLuaStateMap.Add(filename, luastate);
+		return luastate;
+	}
+
+	public void DestroyManager()
+	{
+		foreach (KeyValuePair<string, IntPtr> pair in mLuaStateMap) {
+			NativeMethods.lua_close(pair.Value);
+		}
+		mLuaStateMap = null;
+
+//		foreach (KeyValuePair<string, DelegateLuaBindFunction> pair in mDelegateAddressMap) {
+//			pair.Value = null;// null入れられない
+//		}
+		mDelegateAddressMap = null;
+	}
+
+	void Update () {
+	}
+
+	// LuaScriptを読み込んで、使える状態にする
+	// 基本は、呼び出し側で使いたいLuaScriptを指定するはずなので、
+	// TextAssetは呼び出し側で作って渡してもらうようにする
+	public void LoadLuaScript(TextAsset textasset)
+	{
+		IntPtr luastate = IntPtr.Zero;
+		// すでに登録されている物か、確認する
+		if(mLuaStateMap.ContainsKey(textasset.name) == false)
+		{
+			luastate = GetLuaState(textasset.name);
+		}
+		else
+		{
+			luastate = mLuaStateMap[textasset.name];
+		}
+		
+		// Lua読み込み
+		int res = NativeMethods.luaL_loadstring (luastate, textasset.text);
+		res = NativeMethods.lua_pcallk (luastate, 0, -1, 0);// 読んだら、一回これよばないと正常に機能しない。
+	}
+	
+	// Luaの関数を使う場合には、引数の数と戻り値の数をこちら側から指定する必要がある
+	public ArrayList Call(string filename, FunctionData fData)
+	{
+		IntPtr luastate = IntPtr.Zero;
+		// すでに登録されている物か、確認する
+		if(mLuaStateMap.ContainsKey(filename) == false)
+		{
+			luastate = GetLuaState(filename);
+		}
+		else
+		{
+			luastate = mLuaStateMap[filename];
+		}
+
+		// スタックの初期化のやり方を一応メモしておく
+		//NativeMethods.lua_settop (luastate, 0);
+
+		// stateにスタックを積んでいく
+		// 関数Add
+		NativeMethods.lua_getglobal(luastate, fData.functionName);
+
+		// 引数Add
+		for(int i = 0; i < fData.argList.Count; i++)
+		{
+			var val = fData.argList[i];
+
+			if(val == null)
+			{
+				NativeMethods.lua_pushnil(luastate);
+			}
+			else if(val is int)
+			{
+				NativeMethods.lua_pushvalue(luastate, (int)val);
+			}
+			else if(val is string)
+			{
+				NativeMethods.lua_pushstring(luastate, val as string);
+			}
+			else if(val is IntPtr)
+			{
+				NativeMethods.lua_pushstdcallcfunction(luastate, (IntPtr)val);
+			}
+			else if(val is double)
+			{
+				NativeMethods.lua_pushnumber(luastate, (double)val);
+			}
+			else if(val is float)
+			{
+				double arg = (float)val;
+				NativeMethods.lua_pushnumber(luastate, arg);
+			}
+			else if(val is bool)
+			{
+				int boolnum = 0;
+				if((bool)val == true)
+				{
+					boolnum = 1;
+				}
+				else
+				{
+					boolnum = 0;
+				}
+				NativeMethods.lua_pushboolean(luastate, boolnum);
+			}
+			else if(val is TableData)
+			{
+				// とりあえず、文字列のみ登録できるようにしている
+				int top = NativeMethods.lua_gettop(luastate);
+				NativeMethods.lua_createtable(luastate, top, 1);
+				for(int j = 0; j < (val as TableData).tableDataList.Count; j++)
+				{
+					int tablecount = 0;
+					NativeMethods.lua_pushstring(luastate, (j+1).ToString());
+					tablecount++;
+					NativeMethods.lua_pushstring(luastate, (val as TableData).tableDataList[j] as String);
+					tablecount++;
+
+					NativeMethods.lua_settable(luastate, -(tablecount+1));// テーブルに登録した数+テーブル本体
+				}
+			}
+		}
+		int res = NativeMethods.lua_pcallk (luastate, fData.argList.Count, fData.returnValueNum, 0);
+		ArrayList returnList = new ArrayList();
+		getStack(luastate, returnList);
+
+		return returnList;
+	}
+	
+	public void getStack(IntPtr luastate, ArrayList list)
+	{
+		int num = NativeMethods.lua_gettop (luastate);
+		Debug.Log ("count = " + num);
+		
+		if(num==0)
+		{
+			return;
+		}
+		
+		for(int i = num; i >= 1; i--)
+		{
+			int type = NativeMethods.lua_type(luastate, i);
+
+			switch(type) {
+			case 0://LuaTypes.LUA_TNIL:
+				break;
+			case 1://LuaTypes.LUA_TBOOLEAN:
+				int res_b = NativeMethods.lua_toboolean(luastate, i);
+				list.Add(res_b);
+				break;
+			case 2://LuaTypes.LUA_TLIGHTUSERDATA:
+				break;
+			case 3://LuaTypes.LUA_TNUMBER:
+				double res_d = NativeMethods.lua_tonumberx(luastate, i, 0);
+				list.Add(res_d);
+				break;
+			case 4://LuaTypes.LUA_TSTRING:
+				uint res;
+				IntPtr res_s = NativeMethods.lua_tolstring(luastate, i, out res);
+				string resString = Marshal.PtrToStringAnsi(res_s);
+				list.Add(resString);
+				break;
+			case 5://LuaTypes.LUA_TTABLE:
+				TableData tdata = new TableData();
+				// まず、長さが指定されているはずなので、取得する
+				NativeMethods.lua_getfield (luastate, i, "length");
+				int tableLength = (int)NativeMethods.lua_tonumberx (luastate, i+1, 0);
+				for (int j = 0; j < tableLength; j++) 
+				{
+					// テーブルだけスタックに積んでおきたいので、トップの位置調整
+					NativeMethods.lua_settop (luastate, i);
+					
+					// テーブルから、添え字に見立てた文字列からデータを取得
+					NativeMethods.lua_getfield (luastate, i, j.ToString());
+					
+					// コントロールネームをスタックに積む
+					uint r;
+					IntPtr r_s = NativeMethods.lua_tolstring(luastate, i+1, out r);
+					string text = Marshal.PtrToStringAnsi(r_s);
+					tdata.tableDataList.Add(text);
+					Debug.Log (text);
+				}
+				list.Add(tdata);
+				break;
+			case 6://LuaTypes.LUA_TFUNCTION:
+				break;
+			case 7://LuaTypes.LUA_TUSERDATA:
+				break;
+			//case LuaTypes.LUA_TTHREAD:
+			//	break;
+			}
+		}
+	}
+	
+	public void printStack(IntPtr luastate)
+	{
+		int num = NativeMethods.lua_gettop (luastate);
+		Debug.Log ("count = " + num);
+		if(num==0)
+		{
+			return;
+		}
+		
+		for(int i = num; i >= 1; i--)
+		{
+			int type = NativeMethods.lua_type(luastate, i);
+
+			switch(type) {
+			case 0://LuaTypes.LUA_TNIL:
+				Debug.Log ("stack:"+i+" LUA_TNIL");
+				break;
+			case 1://LuaTypes.LUA_TBOOLEAN:
+				int res_b = NativeMethods.lua_toboolean(luastate, i);
+				Debug.Log ("stack:"+i+" LUA_TBOOLEAN : " + res_b);
+				break;
+			case 2://LuaTypes.LUA_TLIGHTUSERDATA:
+				break;
+			case 3://LuaTypes.LUA_TNUMBER:
+				double res_d = NativeMethods.lua_tonumberx(luastate, i, 0);
+				Debug.Log ("stack:"+i+" LUA_TNUMBER : " + res_d);
+				break;
+			case 4://LuaTypes.LUA_TSTRING:
+				uint res;
+				IntPtr res_s = NativeMethods.lua_tolstring(luastate, i, out res);
+				string resString = Marshal.PtrToStringAnsi(res_s);
+				Debug.Log ("stack:"+i+" LUA_TSTRING : " + resString);
+				break;
+			case 5://LuaTypes.LUA_TTABLE:
+				Debug.Log ("stack:"+i+" LUA_TTABLE");
+				break;
+			case 6://LuaTypes.LUA_TFUNCTION:
+				Debug.Log ("stack:"+i+" LUA_TFUNCTION");
+				break;
+			case 7://LuaTypes.LUA_TUSERDATA:
+				Debug.Log ("stack:"+i+" LUA_TUSERDATA");
+				break;
+			//case LuaTypes.LUA_TTHREAD:
+			//	break;
+			}
+		}
+	}
+	
+	// Lua側にUnityの関数を教える
+	// Lua側からUnityの関数を呼び出す時に、ガベコレで関数デリゲートを持ってかれないように参照も保持する
+	public void AddUnityFunction(string filename, string functionName, IntPtr functionPointer, DelegateLuaBindFunction function)
+	{
+		IntPtr luastate = GetLuaState (filename);
+		NativeMethods.lua_pushcclosure (luastate, functionPointer, 0);
+		NativeMethods.lua_setglobal (luastate, functionName);
+
+		mDelegateAddressMap.Add(filename+functionName, function);
+	}
+}
---
+++ UseLuaForUnityControl/LuaBind/UnityUtility.cs
@@ -0,0 +1,300 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using LuaDLLTest;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.IO;
+
+public class UnityUtility : SingletonMonoBehaviour<UnityUtility> {
+
+	IntPtr mLuaState;
+	GCHandle gcHandle;
+
+	LuaManager.DelegateLuaBindFunction method1 = null;
+	
+	// Update関数を呼び出したいLuaスクリプトのマップ
+	Dictionary<string, string> mLuaCallUpdateMap = null;
+
+	string scriptName = "UnityBind";
+
+	void Awake () {
+		base.Awake ();
+		DontDestroyOnLoad (this);
+	}
+	
+	void Update () {
+		if(mLuaCallUpdateMap != null)
+		{
+			foreach (KeyValuePair<string, string> pair in mLuaCallUpdateMap) {
+				// LuaにUpdateを通知する
+				LuaManager.FunctionData data = new LuaManager.FunctionData();
+				data.returnValueNum = 0;
+				data.functionName = "Update";
+				ArrayList list = new ArrayList();
+				data.argList = list;
+				ArrayList returnList = LuaManager.Instance.Call(pair.Value, data);
+			}
+		}
+	}
+
+	public void Init()
+	{
+		mLuaCallUpdateMap = new Dictionary<string, string>();
+		
+		LuaManager.Instance.Init ();
+		TextAsset file = Resources.Load<TextAsset>(scriptName);
+
+		// まずは、スクリプトをロードして使える状態にする
+		LuaManager.Instance.LoadLuaScript (file);
+		
+		// Unity関数をLua側に登録する
+		BindCommonFunction (scriptName);
+
+		// LuaのCsvManager初期化処理
+		LuaManager.DelegateLuaBindFunction LuaUnityInitCsvManager = new LuaManager.DelegateLuaBindFunction (UnityInitCsvManager);
+		IntPtr LuaUnityInitCsvManagerIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityInitCsvManager);
+		LuaManager.Instance.AddUnityFunction(file.name, "UnityInitCsvManager", LuaUnityInitCsvManagerIntPtr, LuaUnityInitCsvManager);
+
+/*		mLuaState = NativeMethods.luaL_newstate();
+		NativeMethods.luaL_openlibs(mLuaState);
+
+		DelegateLuaBindFunction LuaUnityLoadLevel = new DelegateLuaBindFunction (UnityLoadLevel);
+		IntPtr LuaUnityLoadLevelIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityLoadLevel);
+		//関数ポインタが指しているデリゲートがGCに回収されないようメモリを確保するらしい
+//		gcHandle = GCHandle.Alloc(LuaUnityLoadLevel);
+
+		// ここはアセットバンドルで読み込んだリソースを使う方向に変更する必要があると思うよ
+		TextAsset file = Resources.Load<TextAsset>("UnityBind");
+		NativeMethods.luaL_loadstring (mLuaState, file.text);
+		NativeMethods.lua_pcallk (mLuaState, 0, -1, 0);
+
+		NativeMethods.lua_pushcclosure (mLuaState, LuaUnityLoadLevelIntPtr, 0);
+		NativeMethods.lua_setglobal (mLuaState, "UnityLoadLevel");
+		
+		// 関数呼び出したと仮定
+		NativeMethods.lua_getglobal(mLuaState, "LuaLoadLevel");
+		NativeMethods.lua_pushstring(mLuaState, "NextScene");
+		int res = NativeMethods.lua_pcallk (mLuaState, 1, 0, 0);*/
+
+		// Lua側のメイン関数を呼び出す
+		LuaManager.FunctionData data = new LuaManager.FunctionData();
+		data.returnValueNum = 0;
+		data.functionName = "LuaMain";
+		ArrayList list = new ArrayList();
+		data.argList = list;
+		ArrayList returnList = LuaManager.Instance.Call(scriptName, data);
+
+	}
+
+	// プレハブを読み込んでシーンに追加する。ファイル名はLua側から渡される
+	int UnityLoadPrefab(IntPtr luaState)
+	{
+		Debug.Log ("UnityLoadPrefab");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luaState, 1, out res);
+		string prefabname = Marshal.PtrToStringAnsi(res_s);
+		Debug.Log (prefabname);
+		
+		string ext = Path.GetExtension(prefabname);
+		UnityEngine.Object obj = Resources.Load(prefabname.Substring(0, prefabname.Length - ext.Length), typeof(GameObject));
+		GameObject retObj = UnityEngine.Object.Instantiate(obj) as GameObject;
+		GameObject root = GameObject.Find ("UIRoot");
+		retObj.transform.parent = root.transform;
+		retObj.transform.localPosition = retObj.transform.position;
+		retObj.transform.localScale	= Vector3.one;
+		
+		return 0;
+	}
+
+	int UnityLoadLevel(IntPtr luaState)
+	{
+		Debug.Log ("UnityLoadLevel");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luaState, 1, out res);
+		string sceneName = Marshal.PtrToStringAnsi(res_s);
+		Application.LoadLevel (sceneName);
+
+		return 0;
+	}
+	
+	int UnityDebugLog(IntPtr luaState)
+	{
+		Debug.Log ("UnityDebugLog");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luaState, 1, out res);
+		string logstring = Marshal.PtrToStringAnsi(res_s);
+		Debug.Log (logstring);
+
+		return 0;
+	}
+	
+	// Luaファイル読み込み。他のLuaファイルから別のLuaファイルの処理を呼び出したい時に使う
+	int UnityLoadLuaFile(IntPtr luaState)
+	{
+		Debug.Log ("UnityLoadLuaFile");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luaState, 1, out res);
+		string luaFileName = Marshal.PtrToStringAnsi(res_s);
+
+		TextAsset file = Resources.Load<TextAsset>(luaFileName);
+		LuaManager.Instance.LoadLuaScript (file);
+		
+		return 0;
+	}
+
+	// Lua側のCavManagerの初期化
+	int UnityInitCsvManager(IntPtr luaState)
+	{
+		Debug.Log ("UnityInitCsvManager");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luaState, 1, out res);
+		string luaFileName = Marshal.PtrToStringAnsi(res_s);
+
+		// Unity関数をLua側に登録する
+		BindCommonFunction ("CsvManager");
+				
+		LuaManager.DelegateLuaBindFunction LuaUnityLoadCsv = new LuaManager.DelegateLuaBindFunction (UnityLoadCsv);
+		IntPtr LuaUnityLoadCsvIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityLoadCsv);
+		LuaManager.Instance.AddUnityFunction(luaFileName, "UnityLoadCsv", LuaUnityLoadCsvIntPtr, LuaUnityLoadCsv);
+		
+		// LuaのCsvManagerの初期化処理を呼び出す
+		LuaManager.FunctionData datacsv = new LuaManager.FunctionData();
+		datacsv.returnValueNum = 0;
+		datacsv.functionName = "LoadCsv";
+		ArrayList listcsv = new ArrayList();
+		datacsv.argList = listcsv;
+		ArrayList returnListcsv = LuaManager.Instance.Call("CsvManager", datacsv);
+
+		return 0;
+	}
+	
+	int UnityLoadCsv(IntPtr luastate)
+	{
+		Debug.Log ("UnityLoadCsv");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luastate, 1, out res);
+		string csvname = Marshal.PtrToStringAnsi(res_s);
+		Debug.Log (csvname);
+		string ext = Path.GetExtension(csvname);
+		TextAsset csvloadscript = Resources.Load<TextAsset>(csvname.Substring(0, csvname.Length - ext.Length));
+
+		// CSVロード関数を呼び出す
+		LuaManager.FunctionData data = new LuaManager.FunctionData();
+		data.returnValueNum = 0;
+		data.functionName = "ParseCsvResource";
+		ArrayList list = new ArrayList();
+		list.Add (csvloadscript.name);
+		list.Add (csvloadscript.text);
+		data.argList = list;
+		ArrayList returnList = LuaManager.Instance.Call("CsvManager", data);
+		return 0;
+	}
+	
+	// LuaScriptの関数を呼び出す
+	// 主に、別ファイル間で関数を呼び出したい時に使う
+	// 引数対応は、今度する（テーブル渡せば可変で渡せる）
+	int UnityCallLuaFunction(IntPtr luastate)
+	{
+		Debug.Log ("UnityCallLuaFunction");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luastate, 1, out res);
+		string csvname = Marshal.PtrToStringAnsi(res_s);
+		string ext = Path.GetExtension(csvname);
+		csvname = csvname.Substring(0, csvname.Length - ext.Length);
+
+		res_s = NativeMethods.lua_tolstring(luastate, 2, out res);
+		string functionName = Marshal.PtrToStringAnsi(res_s);
+		
+		
+		// 関数を呼び出す
+		LuaManager.FunctionData data = new LuaManager.FunctionData();
+		data.returnValueNum = 0;
+		data.functionName = functionName;
+		ArrayList list = new ArrayList();
+		data.argList = list;
+		ArrayList returnList = LuaManager.Instance.Call(csvname, data);
+		return 0;
+	}
+	
+	// Updateを呼び出したいLuaスクリプトの登録
+	int UnityAddCallUpdateScript(IntPtr luastate)
+	{
+		Debug.Log ("UnityAddCallUpdateScript");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luastate, 1, out res);
+		string csvname = Marshal.PtrToStringAnsi(res_s);
+		string ext = Path.GetExtension(csvname);
+		csvname = csvname.Substring(0, csvname.Length - ext.Length);
+
+		if(mLuaCallUpdateMap.ContainsKey(csvname) == true)
+		{
+			return 0;
+		}
+		
+		mLuaCallUpdateMap.Add(csvname, csvname);
+		return 0;
+	}
+	
+	// どこでも使うUnityの関数を、Luaに登録する
+	int UnityBindCommonFunction(IntPtr luastate)
+	{
+		Debug.Log ("UnityBindCommonFunction");
+		uint res;
+		IntPtr res_s = NativeMethods.lua_tolstring(luastate, 1, out res);
+		string csvname = Marshal.PtrToStringAnsi(res_s);
+		string ext = Path.GetExtension(csvname);
+		csvname = csvname.Substring(0, csvname.Length - ext.Length);
+
+		BindCommonFunction(csvname);
+		return 0;
+	}
+
+	public void BindCommonFunction(string scriptName)
+	{
+		// シーン切り替え
+		LuaManager.DelegateLuaBindFunction LuaUnityLoadLevel = new LuaManager.DelegateLuaBindFunction (UnityLoadLevel);
+		IntPtr LuaUnityLoadLevelIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityLoadLevel);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityLoadLevel", LuaUnityLoadLevelIntPtr, LuaUnityLoadLevel);
+		//gcHandle = GCHandle.Alloc(LuaUnityLoadLevel);// これないと、なぜかLua側でUnityLoadLevel呼び出すとUnityが強制終了する
+		//method1 = new DelegateLuaBindFunction (UnityLoadLevel);
+		//IntPtr LuaUnityLoadLevelIntPtr = Marshal.GetFunctionPointerForDelegate(method1);
+		//LuaManager.Instance.AddUnityFunction(scriptName, "UnityLoadLevel", LuaUnityLoadLevelIntPtr);
+
+		// これは、ガベコレタイミングを遅らせるだけらしいので、解決にはならんから使わない
+		//GC.KeepAlive (LuaUnityLoadLevel);
+		//GC.KeepAlive (LuaUnityLoadLevelIntPtr);
+		
+		// デバッグログ
+		LuaManager.DelegateLuaBindFunction LuaUnityDebugLog = new LuaManager.DelegateLuaBindFunction (UnityDebugLog);
+		IntPtr LuaUnityDebugLogIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityDebugLog);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityDebugLog", LuaUnityDebugLogIntPtr, LuaUnityDebugLog);
+
+		// Luaファイル読み込み処理
+		LuaManager.DelegateLuaBindFunction LuaUnityLoadLuaFile = new LuaManager.DelegateLuaBindFunction (UnityLoadLuaFile);
+		IntPtr LuaUnityLoadLuaFileIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityLoadLuaFile);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityLoadLuaFile", LuaUnityLoadLuaFileIntPtr, LuaUnityLoadLuaFile);
+		
+		// プレハブの読み込み処理
+		LuaManager.DelegateLuaBindFunction LuaUnityLoadPrefab = new LuaManager.DelegateLuaBindFunction (UnityLoadPrefab);
+		IntPtr LuaUnityLoadPrefabIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityLoadPrefab);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityLoadPrefab", LuaUnityLoadPrefabIntPtr, LuaUnityLoadPrefab);
+		
+		// Update（更新処理）登録
+		LuaManager.DelegateLuaBindFunction LuaUnityAddCallUpdateScript = new LuaManager.DelegateLuaBindFunction (UnityAddCallUpdateScript);
+		IntPtr LuaUnityAddCallUpdateScriptIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityAddCallUpdateScript);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityAddCallUpdateScript", LuaUnityAddCallUpdateScriptIntPtr, LuaUnityAddCallUpdateScript);
+		
+		// 関数呼び出し
+		LuaManager.DelegateLuaBindFunction LuaUnityCallLuaFunction = new LuaManager.DelegateLuaBindFunction (UnityCallLuaFunction);
+		IntPtr LuaUnityCallLuaFunctionIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityCallLuaFunction);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityCallLuaFunction", LuaUnityCallLuaFunctionIntPtr, LuaUnityCallLuaFunction);
+		
+		// コモン関数の登録
+		LuaManager.DelegateLuaBindFunction LuaUnityBindCommonFunction = new LuaManager.DelegateLuaBindFunction (UnityBindCommonFunction);
+		IntPtr LuaUnityBindCommonFunctionIntPtr = Marshal.GetFunctionPointerForDelegate(LuaUnityBindCommonFunction);
+		LuaManager.Instance.AddUnityFunction(scriptName, "UnityBindCommonFunction", LuaUnityBindCommonFunctionIntPtr, LuaUnityBindCommonFunction);
+	}
+
+}
+
