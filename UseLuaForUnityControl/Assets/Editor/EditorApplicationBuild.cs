aa65a9b0e9008a85bb3240f45e2b5160040dc6c4
yamashita <wisdomword-justice@hotmail.co.jp>
2017-04-29 21:28:51 +0900

    ビルドスクリプト追加

---
+++ UseLuaForUnityControl/Assets/Editor/EditorApplicationBuild.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EditorApplicationBuild {
+
+	// ビルド実行でAndroidのapkを作成する例
+	[UnityEditor.MenuItem("Tools/Build Android")]
+	public static void BuildAndroid() {
+		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+		List<string> allScene = new List<string>();
+		foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+			if (scene.enabled) {
+				allScene.Add (scene.path);
+			}
+		}
+		//PlayerSettings.bundleIdentifier = "com.yourcompany.newgame";
+		//PlayerSettings.statusBarHidden = true;
+		BuildPipeline.BuildPlayer( 
+			allScene.ToArray(),
+			"Mofuneko.apk",
+			BuildTarget.Android,
+			BuildOptions.None
+		);
+	}
+}
